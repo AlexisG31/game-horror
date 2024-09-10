1a2c137b5ddae06bc1649cb7c717edb37d452a4a
Dev5001 <136515887+Dev5001@users.noreply.github.com>
2024-09-09 22:19:50 -0700

    Basic Pathfinding + Monster Movement

---
+++ Deep Space/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterMovement : MonoBehaviour
+{
+    [SerializeField]
+    NavMeshAgent monster;
+    [SerializeField]
+    GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        monster.SetDestination(target.transform.position);
+    }
+}
