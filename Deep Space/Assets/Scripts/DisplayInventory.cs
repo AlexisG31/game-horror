57791274d1939d1a40d5d760572927862f6ff70b
AlexisG31 <agarcia71901@gmail.com>
2024-10-18 14:32:16 -0700

    E

--- Deep Space/Assets/Scripts/DisplayInventory.cs
+++ Deep Space/Assets/Scripts/DisplayInventory.cs
@@ -6,7 +6,8 @@ using TMPro;
 public class DisplayInventory : MonoBehaviour
 {
     public InventoryObject Inventory;
-
+    public int X_START;
+    public int Y_START;
     public int X_SPACE_BETWEEN_ITEM;
     public int NUMBER_OF_COLUMN;
     public int Y_SPACE_BETWEEN_ITEMS;
@@ -20,7 +21,7 @@ public class DisplayInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //UpdateDisplay();
+        UpdateDisplay();
     }
     public void CreateDisplay()
     {
@@ -29,10 +30,31 @@ public class DisplayInventory : MonoBehaviour
             var obj = Instantiate(Inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = Inventory.Container[i].amount.ToString("n0");
+<<<<<<< Updated upstream
+=======
+            itemsDisplayed.Add(Inventory.Container[i], obj);
+>>>>>>> Stashed changes
         }
     }
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(X_SPACE_BETWEEN_ITEM *(i % NUMBER_OF_COLUMN), (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)), 0f);
+        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM *(i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)), 0f);
+    }
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < Inventory.Container.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(Inventory.Container[i]))
+            {
+                itemsDisplayed[Inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = Inventory.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+            var obj = Instantiate(Inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = Inventory.Container[i].amount.ToString("n0");
+            itemsDisplayed.Add(Inventory.Container[i], obj);
+            }
+        }
     }
 }
