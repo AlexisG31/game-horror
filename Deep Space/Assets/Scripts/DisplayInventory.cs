a7dcda6b775fa85b1500abef97d9f35fa63074e7
AlexisG31 <agarcia71901@gmail.com>
2024-10-18 17:01:12 -0700

    First part done

--- Deep Space/Assets/Scripts/DisplayInventory.cs
+++ Deep Space/Assets/Scripts/DisplayInventory.cs
@@ -6,11 +6,12 @@ using TMPro;
 public class DisplayInventory : MonoBehaviour
 {
     public InventoryObject Inventory;
-    public int X_START;
-    public int Y_START;
+
     public int X_SPACE_BETWEEN_ITEM;
     public int NUMBER_OF_COLUMN;
     public int Y_SPACE_BETWEEN_ITEMS;
+    public int X_START;
+    public int Y_START;
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
     // Start is called before the first frame update
     void Start()
@@ -30,15 +31,12 @@ public class DisplayInventory : MonoBehaviour
             var obj = Instantiate(Inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = Inventory.Container[i].amount.ToString("n0");
-<<<<<<< Updated upstream
-=======
             itemsDisplayed.Add(Inventory.Container[i], obj);
->>>>>>> Stashed changes
         }
     }
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM *(i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)), 0f);
+        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM *(i % NUMBER_OF_COLUMN)),Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)), 0f);
     }
     public void UpdateDisplay()
     {
